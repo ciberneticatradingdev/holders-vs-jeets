0a6060354707a3f8aec080e1b5e857ba3ec01a16
ciberneticatradingdev <ciberneticatrading@gmail.com>
2026-06-19 07:39:48 -0400

    chore: remove node_modules and unused root files; add .gitignore

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GameApp } from './components/GameApp'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GameApp />
-  </React.StrictMode>
-)
